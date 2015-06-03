b9ab11b12a6e2a3d9548467044c71b466d97175c
ayahc1093 <ayahc10.93@gmail.com>
2015-06-02 21:23:53 -0500

    Initial commit

---
+++ app/src/main/java/com/example/mcberliner/notificationdemo/NotificationRecieverActivity.java
@@ -0,0 +1,16 @@
+package com.example.mcberliner.notificationdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by mcberliner on 6/1/2015.
+ */
+public class NotificationRecieverActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notification);
+    }
+}
